1508ff78acbea77dd16dbd40a4cad400cd4d5c09
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-26 19:48:16 -0800

    added the basic react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      Hello 
+    </div>
+  );
+}
+
+export default App;
