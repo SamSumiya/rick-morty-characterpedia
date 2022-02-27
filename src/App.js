bc4b138107d29e82fd59fa77fb9ceebfa856ffbf
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-26 22:24:01 -0800

    added fetch data function and display it on the screen with grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
+// Imported from local folders and files
+import { useCharacters } from './hooks/useCharacters';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Imported from outside resoucrs
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const { fetchedAllCharacters, totalPageNumbers, isLoading, setIsLoading } =
+    useCharacters(2, '', 'dead', 'female');
+  console.log(fetchedAllCharacters, 'page: ', totalPageNumbers);
   return (
-    <div className="App">
-      Hello 
+    <div className="displayingCharacters-wrapper">
+      {fetchedAllCharacters.map((character) => (
+        <ul key={character.id} className="displayCharacters-character">
+          <img src={character.image} alt={character.image} />
+          <li>{character.name}</li>
+          <li>{character.status}</li>
+          <li>{character.species}</li>
+        </ul>
+      ))}
     </div>
   );
 }
