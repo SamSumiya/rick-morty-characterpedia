04757e3488014fee625665cb1758e2737d93d150
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-28 13:57:41 -0800

    added search function to have a dropdown and use can select the value from it to see the selected character

--- src/components/FilterCharacters.jsx
+++ src/components/FilterCharacters.jsx
@@ -13,12 +13,31 @@ export const FilterCharacters = ({
     event.preventDefault();
   };
 
+  const handleChange = (value) => {
+    setSearch(value);
+  };
+
+
+  console.log(search);
+  setUsernameInput(search);
+  
   const handleFilter = (event) => {
     event.preventDefault();
+
     const searchTerm = event.target.value;
+    console.log(searchTerm, 'searchTerm');
+
+
     setSearch(searchTerm);
-    setUsernameInput(search);
-    if (searchTerm === '' || searchTerm === null) {
+    console.log(search)
+    // setUsernameInput(search);
+
+    if (
+      searchTerm === '' ||
+      searchTerm === null ||
+      search === '' ||
+      search === null
+    ) {
       setFilteredCharacters([]);
     } else {
       const filteredCharacters = fetchedAllCharacters.filter((character) => {
@@ -32,23 +51,42 @@ export const FilterCharacters = ({
     setFilterCharacters(filteredCharacters);
   }, [filteredCharacters, setFilterCharacters]);
 
+  // const newCharacters = [...fetchedAllCharacters]
+  // const justNames = [] 
+  // filteredCharacters.map((character) => {
+  //   return justNames.push(character.name);
+  // }); 
+
+  // console.log(justNames, 'justName');
+  // const currentIndex = justNames.indexOf(search); 
+  // console.log(currentIndex, 'currentIndex'); 
+
   return (
     <form className="input-wrapper" onSubmit={handleFormSubmit}>
       <input
-        type="text"
+        type="search"
         placeholder="Search for a character"
         value={search}
         onChange={handleFilter}
       />
-     {filteredCharacters.length !== 0 && <div className="search-characterNames">
-        {filteredCharacters.slice(0, 5).map((character) => {
-          return (
-            <div key={character.id}>
-              <p className="dropdown-name">{character.name}</p>
-            </div>
-          );
-        })}
-      </div>}
+      {filteredCharacters.length !== 0 && (
+        <div className="search-characterNames">
+          {filteredCharacters
+            // .slice(0, currentIndex+2)
+            .map((character) => {
+              return (
+                <div key={character.id}>
+                  <p
+                    className="dropdown-name"
+                    onClick={(value) => handleChange(value.target.innerText)}
+                  >
+                    {character.name}
+                  </p>
+                </div>
+              );
+            })}
+        </div>
+      )}
       <button onClick={handleFormSubmit}>Search</button>
     </form>
   );
