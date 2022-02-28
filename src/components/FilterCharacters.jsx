b3d279f87689d39cb7dd06df897f0d97619f6aa2
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-28 10:37:22 -0800

    added search and a dropdown menu of the possible mathces

--- src/components/FilterCharacters.jsx
+++ src/components/FilterCharacters.jsx
@@ -1,35 +1,54 @@
 import '../styles/FilterCharacters.css';
 import { useState, useEffect } from 'react';
 
-export const FilterCharacters = ({ setUsernameInput, fetchedAllCharacters }) => {
+export const FilterCharacters = ({
+  setUsernameInput,
+  fetchedAllCharacters,
+  setFilterCharacters,
+}) => {
   const [search, setSearch] = useState('');
+  const [filteredCharacters, setFilteredCharacters] = useState([]);
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
   };
 
-  useEffect(() => {
+  const handleFilter = (event) => {
+    event.preventDefault();
+    const searchTerm = event.target.value;
+    setSearch(searchTerm);
     setUsernameInput(search);
-  }, [search, setUsernameInput]);
-
-  const handleChange = (event) => {
-    setSearch(event.target.value);
+    if (searchTerm === '' || searchTerm === null) {
+      setFilteredCharacters([]);
+    } else {
+      const filteredCharacters = fetchedAllCharacters.filter((character) => {
+        return character.name.toLowerCase().includes(search.toLowerCase());
+      });
+      setFilteredCharacters(filteredCharacters);
+    }
   };
+
+  useEffect(() => {
+    setFilterCharacters(filteredCharacters);
+  }, [filteredCharacters, setFilterCharacters]);
 
   return (
     <form className="input-wrapper" onSubmit={handleFormSubmit}>
       <input
-        type="search"
+        type="text"
         placeholder="Search for a character"
-        onChange={handleChange}
+        value={search}
+        onChange={handleFilter}
       />
-      {fetchedAllCharacters.map(character => {
-        return (
-          <div key={character.id}>
-            <p>{character.name}</p>
-          </div>
-        )
-      })}
+     {filteredCharacters.length !== 0 && <div className="search-characterNames">
+        {filteredCharacters.slice(0, 5).map((character) => {
+          return (
+            <div key={character.id}>
+              <p className="dropdown-name">{character.name}</p>
+            </div>
+          );
+        })}
+      </div>}
       <button onClick={handleFormSubmit}>Search</button>
     </form>
   );
