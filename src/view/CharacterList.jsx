21d034243672f8ca074d0cee624eb1da6f038e76
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-03-03 10:28:58 -0800

    styled the character status

--- src/view/CharacterList.jsx
+++ src/view/CharacterList.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../styles/CharacterList.css';
 import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+ import { faInfo } from '@fortawesome/free-solid-svg-icons';
+import { faGrin } from '@fortawesome/free-solid-svg-icons';
+import { faSkull } from '@fortawesome/free-solid-svg-icons';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
 
 export const CharacterList = ({ image, name, status, species, type }) => {
   const [clickForMore, setClickForMore] = React.useState(false);
-
- 
 
   const handleClickForMore = () => {
     setClickForMore(!clickForMore);
@@ -33,13 +36,43 @@ export const CharacterList = ({ image, name, status, species, type }) => {
           <li className="displayCharacters-type">
             {type === ''
               ? 'Type: None'
-              : `Type: ${clickForMore ? type : type.slice(0, 3)}`}
-            <div onClick={handleClickForMore} className='showmore-showless'>
-              {type==='' ? '': clickForMore ? '...more' : '...less'} 
+              : `Type: ${clickForMore ? type : type.slice(0, 5)}`}
+            <div onClick={handleClickForMore} className="showmore-showless">
+              {type === '' ? '' : clickForMore ? '...more' : '...less'}
             </div>
           </li>
-          <li className="displayCharacters-status">{status}</li>
-          <button> Details </button>
+          <li className="displayCharacters-status">
+            {status === 'Alive' ? (
+              <p style={{ color: '#b3e680' }}>
+                <FontAwesomeIcon icon={faGrin} style={{ color: '#e1ffc5' }} />
+                <span style={{ margin: '7px' }}>{'Alive'}</span>
+              </p>
+            ) : (
+              <p>
+                {status === 'Dead' ? (
+                  <p style={{ color: '#e68080' }}>
+                    <FontAwesomeIcon
+                      icon={faSkull}
+                      style={{ color: '#ea9999' }}
+                    />
+                    <span style={{ margin: '7px' }}>{'Dead'}</span>
+                  </p>
+                ) : (
+                  <p style={{ color: '#b680e6' }}>
+                    <FontAwesomeIcon
+                      icon={faQuestion}
+                      style={{ color: '#ccabea' }}
+                    />
+                    <span style={{ margin: '7px' }}>{'Unknown'}</span>
+                  </p>
+                )}{' '}
+              </p>
+            )}
+          </li>
+          <button>
+            <FontAwesomeIcon icon={faInfo} />
+            <span>Details</span>
+          </button>
         </ul>
       }
     </motion.div>
