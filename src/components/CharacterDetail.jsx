b085d90e1e7319980de31bbaa722beca1cf363a4
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-03-04 14:17:05 -0800

    styled the detail page with grid and flex box and somewhat responsive

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,19 +1,43 @@
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 import { useCharacterDetailInfo } from '../hooks/useCharacterDetailInfo';
 import { CharacterDetailsPage } from '../view/CharacterDetailsPage';
 
 export const CharacterDetail = () => {
+  const { characterId } = useParams();
 
- 
-  const { characterId } = useParams(); 
-  console.log(characterId); 
-  const { isLoadingDetail, selectedCharacter } = useCharacterDetailInfo(characterId);
-  
-  if (isLoadingDetail) { 
-    return <div>Loading...</div> 
-  } 
-  
-  return <CharacterDetailsPage selectedCharacter={selectedCharacter} />; 
+  const { isLoadingDetail, selectedCharacter } =
+    useCharacterDetailInfo(characterId);
 
-
+  if (isLoadingDetail) {
+    return (
+      <div>
+        <img
+          src="https://cdn.dribbble.com/users/1944099/screenshots/6385450/rik.gif"
+          alt="nodding Rick!"
+        />
+      </div>
+    );
+  }
+  return (
+    <CharacterDetailsPage
+      createdTime={selectedCharacter.created}
+      episode={selectedCharacter.episode}
+      gender={selectedCharacter.gender}
+      image={
+        selectedCharacter.image
+          ? selectedCharacter.image
+          : 'https://toppng.com/uploads/preview/image-1-source-rick-and-morty-11562982914hmlrrghjuu.png'
+      }
+      name={selectedCharacter.name}
+      species={selectedCharacter.species}
+      status={selectedCharacter.status}
+      nextCharacter={selectedCharacter.url}
+      origin={
+        selectedCharacter.origin ? selectedCharacter.origin.name : 'Loading'
+      }
+      location={
+        selectedCharacter.location ? selectedCharacter.location.name : 'Loading'
+      }
+    />
+  );
 };
--- src/view/CharacterDetailsPage.jsx
+++ src/view/CharacterDetailsPage.jsx
@@ -1,6 +1,101 @@
-import React from 'react'
+import React from 'react';
+import '../styles/CharacterDetails.css';
 
-export const CharacterDetailsPage = ({ selectedCharacter }) => {
-  console.log(selectedCharacter);
-  return <div>CharacterDetails</div>;
+export const CharacterDetailsPage = ({
+  createdTime,
+  episode,
+  gender,
+  image,
+  name,
+  species,
+  status,
+  origin,
+  location,
+  // nextCharacter,
+}) => {
+  const [showMoreOrLess, setShowMoreOrLess] = React.useState(false);
+
+  const handleShowMoreOrLess = () => {
+    setShowMoreOrLess(!showMoreOrLess);
+  };
+
+  return (
+    <div className="character-wrapper">
+      <div className="character-detail-wrapper ">
+        <div className="character-intro">
+          <img
+            src={image}
+            alt="rick and morty character"
+            className="character-intro_image"
+          />
+          <div className="character-intro_name">{name}</div>
+        </div>
+        <div
+          style={{
+            borderBottom: '1px solid #3a3a3a',
+            width: '90%',
+            margin: '1rem auto',
+          }}
+        ></div>
+        <div className="character-Detail">
+          <div className="character-info"> Info </div>
+          <div className="character-detail-listInfo">
+            <div className="character-status">
+              Status: <span> {status} </span>
+            </div>
+            <div className="character-gender">
+              Gender:<span> {gender} </span>
+            </div>
+            <div className="character-species">
+              Species: <span> {species} </span>
+            </div>
+            <div className="character-origin">
+              Origin: <span>{origin} </span>
+            </div>
+            <div className="character-location-info">
+              Last Seen: <span>{location}</span>
+            </div>
+            <div className="character-createdTime">
+              CreatedTime: {createdTime?.slice(0, 10)}
+            </div>
+          </div>
+        </div>
+
+        {/* <button>Next</button> */}
+      </div>
+      <div className="Character-episodes">
+        <div className="Character-episodes-title">
+          Total Episodes: {episode?.length}
+        </div>
+        {showMoreOrLess
+          ? episode?.map((item, index) => {
+              return (
+                <ul key={index}>
+                  <a href={item} style={{ color: '#a9a8a8' }}>
+                    {item}
+                  </a>
+                </ul>
+              );
+            })
+          : episode?.slice(0, 5).map((item, index) => {
+              return (
+                <ul key={index}>
+                  <a href={item} style={{ color: '#a9a8a8' }}>
+                    {item}
+                  </a>
+                </ul>
+              );
+            })}
+        <span onClick={handleShowMoreOrLess}>
+          {showMoreOrLess
+            ? '...less'
+            : episode?.legnth > 5
+            ? '...less'
+            : episode?.length < 5
+            ? ''
+            : '...more'}
+        </span>
+      </div>
+    </div>
+  );
 };
