2ebf6cdb2730c2049329b2481a6df532318cad27
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-03-22 11:58:22 -0700

    added return for filter

--- src/components/FilterCharacters.jsx
+++ src/components/FilterCharacters.jsx
@@ -112,7 +112,7 @@ export const FilterCharacters = ({
       if (!arrayOfNames.includes(character.name)) {
         return arrayOfNames.push(character.name);
       }
-      setNonDupCharacters(arrayOfNames);
+      return setNonDupCharacters(arrayOfNames);
     });
     setIsLoading(false);
   }, [search, fetchedAllCharacters, setIsLoading]);
